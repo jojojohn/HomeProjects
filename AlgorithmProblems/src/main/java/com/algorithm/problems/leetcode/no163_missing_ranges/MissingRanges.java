6d6ab7b80ef8ad2efd9c49f6eda32c9e55967174
Jojo John <jojo.john84@gmail.com>
2020-02-03 08:04:00 -0800

    Missing Ranges : No 163 Implemented the logic and test cases

---
+++ AlgorithmProblems/src/main/java/com/algorithm/problems/leetcode/no163_missing_ranges/MissingRanges.java
@@ -0,0 +1,43 @@
+package com.algorithm.problems.leetcode.no163_missing_ranges;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MissingRanges {
+
+    public List<String> findMissingRanges(final int[] nums, int lower, int upper) {
+        int start = lower;
+        List<String> result = new ArrayList<>();
+        if(start == Integer.MAX_VALUE){
+            return result;
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            if(i<nums.length-1 && nums[i]==nums[i+1]){
+                continue;
+            }
+            if(start == nums[i]) {
+                start++;
+            } else {
+                result.add(generateResult(start, nums[i] - 1));
+                if(nums[i] == Integer.MAX_VALUE){
+                    return result;
+                }
+                start = nums[i] + 1;
+            }
+        }
+        if(start <= upper) {
+            result.add(this.generateResult(start, upper));
+        }
+        return result;
+    }
+
+    private String generateResult(final int start, final int end) {
+        if (start == end) {
+            return String.valueOf(start);
+        } else {
+            return start + "->" + end;
+        }
+    }
+
+}
---
+++ AlgorithmProblems/src/test/java/com/algorithm/problems/leetcode/no163_missing_ranges/MissingRangesTest.java
@@ -0,0 +1,57 @@
+package com.algorithm.problems.leetcode.no163_missing_ranges;
+
+import com.sun.source.tree.AssertTree;
+import junit.framework.TestCase;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class MissingRangesTest extends TestCase {
+
+    private MissingRanges missingRanges = new MissingRanges();
+    public void testFindMissingRanges() {
+        int[] input = {1, 2, 3, 4, 5};
+        int low = 0;
+        int high = 10;
+        List<String> expected = new ArrayList<>();
+        expected.add("0");
+        expected.add("6->10");
+        assertTrue(this.isTwoListsAreEqual(expected, missingRanges.findMissingRanges(input, low, high)));
+        input = new int[]{0, 1, 3, 50, 75};
+        low = 0;
+        high = 99;
+        expected.clear();
+        expected.add("2");
+        expected.add("4->49");
+        expected.add("51->74");
+        expected.add("76->99");
+        assertTrue(this.isTwoListsAreEqual(expected, missingRanges.findMissingRanges(input, low, high)));
+
+        input = new int[]{0, 1, 3, 3, 3, 50, 50, 60, 75};
+        low = 0;
+        high = 99;
+        expected.clear();
+        expected.add("2");
+        expected.add("4->49");
+        expected.add("51->59");
+        expected.add("61->74");
+        expected.add("76->99");
+        assertTrue(this.isTwoListsAreEqual(expected, missingRanges.findMissingRanges(input, low, high)));
+
+        input = new int[]{-2147483648,-2147483648,0,2147483647,2147483647};
+        low = -2147483648;
+        high = 2147483647;
+        expected.clear();
+        expected.add("-2147483647->-1");
+        expected.add("1->2147483646");
+        assertTrue(this.isTwoListsAreEqual(expected, missingRanges.findMissingRanges(input, low, high)));
+
+    }
+
+    public boolean isTwoListsAreEqual(final List<String> list1, final List<String> list2) {
+        Collections.sort(list1);
+        Collections.sort(list2);
+        return list1.equals(list2);
+    }
+}
