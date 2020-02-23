7e1f5e106ed4151e7abeabce6e653805d0362e12
Jojo John <jojo.john84@gmail.com>
2020-02-22 23:47:44 -0800

    KnapSack dynamic programming

--- AlgorithmProblems/src/main/java/com/algorithm/problems/knapsack/KnapSackProblem.java
+++ AlgorithmProblems/src/main/java/com/algorithm/problems/knapsack/KnapSackProblem.java
@@ -1,4 +1,4 @@
-package com.algorithm.problems.leetcode.knapsack;
+package com.algorithm.problems.knapsack;
 
 public class KnapSackProblem {
 
@@ -19,5 +19,19 @@ public class KnapSackProblem {
 
         return Math.max(kanpSack(maxWeight - weights[index], weights, values, index + 1) + values[index],
                 kanpSack(maxWeight, weights, values, index + 1));
+    }
+
+    public int knapSackDP(int weight, int[] weights, int[] values) {
+        int[][] cache = new int[weights.length + 1][weight + 1];
+        for (int i = 1; i < cache.length; i++) {
+            for (int j  = 1; j < cache[0].length; j++) {
+                if (weights[i - 1] > j) {
+                    cache[i][j] = cache[i - 1][j];
+                } else {
+                    cache[i][j] = Math.max(cache[i - 1][j], cache[i - 1][j - weights[i - 1]] + values[i  - 1]);
+                }
+            }
+        }
+        return cache[weights.length][weight];
     }
 }
--- AlgorithmProblems/src/test/java/com/algorithm/problems/leetcode/knapsack/KnapSackProblemTest.java
+++ AlgorithmProblems/src/test/java/com/algorithm/problems/leetcode/knapsack/KnapSackProblemTest.java
@@ -1,5 +1,6 @@
 package com.algorithm.problems.leetcode.knapsack;
 
+import com.algorithm.problems.knapsack.KnapSackProblem;
 import junit.framework.TestCase;
 import org.junit.Test;
 
@@ -12,5 +13,23 @@ public class KnapSackProblemTest extends TestCase {
 
         KnapSackProblem  knapSackProblem = new KnapSackProblem();
         assertEquals(225, knapSackProblem.knapSack(10, weights, values));
+    }
+
+    @Test
+    public void testKanpSackDP () {
+        int[] weights = {1, 3, 2, 5, 4};
+        int[] values = {25, 60, 40, 120, 80};
+
+        KnapSackProblem  knapSackProblem = new KnapSackProblem();
+        assertEquals(225, knapSackProblem.knapSackDP(10, weights, values));
+    }
+
+    @Test
+    public void testKanpSackDP_1 () {
+        int[] weights = {3, 4, 6, 5};
+        int[] values = {2, 3, 1, 4};
+
+        KnapSackProblem  knapSackProblem = new KnapSackProblem();
+        assertEquals(6, knapSackProblem.knapSackDP(8, weights, values));
     }
 }
